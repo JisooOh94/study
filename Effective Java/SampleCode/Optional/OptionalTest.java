e8146b4fa3113f1f78c628585505fa0f83edd588
jisoooh <jisoooh94@worksmobile.com>
2020-03-31 22:12:21 +0900

    [EffectiveJava] syntax 수정

--- Effective Java/SampleCode/Optional/OptionalTest.java
+++ Effective Java/SampleCode/Optional/OptionalTest.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
--- Effective Java/SampleCode/한정적 와일드카드/Wildcard_Generic_comp_test.java
+++ Effective Java/SampleCode/한정적 와일드카드/Wildcard_Generic_comp_test.java
@@ -10,4 +10,15 @@ public class Wildcard_Generic_comp_test {
 		Wildcard_Generic_comp.get_Generic(list, 0);
 		Wildcard_Generic_comp.get_Wildcard(list, 0);
 	}
+
+	public static <T> List<? extends T> union(List<? extends T> list1, List<? extends T> list2) {
+		return list1;
+	}
+
+	@Test
+	public void test2() {
+		List<Integer> list_1 = Arrays.asList(1,2,3);
+		List<Double> list_2 = Arrays.asList(1.1, 1.2, 1.3);
+		List<? extends Number> resultList = union(list_1, list_2);
+	}
 }
